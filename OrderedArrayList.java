45ae9f6dafaa0d3876a2dcd3a53f675cb35d9952
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-12-12 17:32:10 -0500

    Update OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -23,5 +23,18 @@ public class OrderedArrayList {
   public int size() {
     return _data.size();
   }
-
+  
+  public void add(int num){
+        boolean isAdded = false; // Flag for breaking for-loop or appending element to the end of the list
+        for(int i = 0 ; i < _data.size() && !isAdded; i++ ){
+            if(_data.get(i) >= num){ 
+                // At the first instance of an element being greater than or equal to than the new value, 
+              // add the value at the index of that element and set isAdded true to break the for-loop
+                _data.add(i, num);
+                isAdded = true;
+            }
+        }
+        
+        if (!isAdded) _data.add(num); // If the new value is greater than all of the elements in the list (and thus was not added) append it to the end
+    }
 }
